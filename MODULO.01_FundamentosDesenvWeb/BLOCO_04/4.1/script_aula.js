6f645dc72b0655e7a289dcdb348d7457de03797d
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-05-19 20:37:06 -0300

    Adiciona os exercicios do conteudo

---
+++ MODULO.01_FundamentosDesenvWeb/BLOCO_04/4.1/script_aula.js
@@ -0,0 +1,61 @@
+// -- EX1
+// const name = "Gustavão";
+// const birthCity = "Sorocaba-SP";
+// let birthYear = 1992;
+
+// -- EX2
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// -- EX3
+// let patientId = "50";
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: "Ana",
+//   lastName: "Santos",
+// };
+// const patientEmail = "ana@email.com";
+
+// console.log(typeof patientId);
+
+// -- EX4
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = [(2*base) + (2*altura)];
+
+// console.log(area);
+// console.log(perimetro);
+
+//-- EX5
+// let notaCandidato = 69;
+
+// if (notaCandidato >= 80) {
+//   console.log("Parabéns! Você foi aprovado (a)")
+// }
+// else if (notaCandidato < 80 && notaCandidato >= 60) {
+//   console.log("Você está na nossa lista de espera")
+// } else {
+//   console.log("Você foi reprovado (a)")
+// }
+
+// -- EX6
+// let statusCandidato = "Lista";
+
+// switch (statusCandidato) {
+//   case "Aprovado":
+//     console.log("Aprovado!! Congrats");
+//     break;
+
+//   case "Lista":
+//     console.log("Você está na nossa lista de espera");
+//     break;
+
+//   case "Reprovado":
+//     console.log("Você foi reprovado");
+//     break;  
+
+//   default:
+//     console.log("Inválido");
+// }
