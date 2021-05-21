5672546504e09489ee93b7a324da5342930c5a69
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-05-21 18:22:55 -0300

    Adiciona primeiro exercício

---
+++ MODULO.01_FundamentosDesenvWeb/BLOCO_04/4.3/script.js
@@ -0,0 +1,18 @@
+let n = 5;
+let simbolo = "*";
+let square = '';
+
+for (let i = 1; i <= n; i += 1) {
+  square += simbolo;
+}
+
+for (let i2 = 1; i2 <= n; i2 += 1) {
+  console.log(square);
+}
+
+
+
+// ler um numero
+// criar a partir dele
+// uma linha com um asterisco para cada unidade dele
+// repetir essa linha o numero dele de vezes
