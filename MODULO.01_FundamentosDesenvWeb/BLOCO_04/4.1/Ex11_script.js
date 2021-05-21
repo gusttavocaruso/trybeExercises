96e3d8f5ae14d5b32362ff58a48ca04321267171
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-05-21 13:52:29 -0300

    Corrige exercício 11 do dia 1 do bloco 4

--- MODULO.01_FundamentosDesenvWeb/BLOCO_04/4.1/Ex11_script.js
+++ MODULO.01_FundamentosDesenvWeb/BLOCO_04/4.1/Ex11_script.js
@@ -1,51 +1,53 @@
 
-let salarioBruto = 5400.10;
+let salarioCarteira = 5400.1;
+let salarioBruto = salarioCarteira;
 let descontoINSS;
 let descontoIR;
 let salarioLiquido;
 let descontoMaxINSS = 570.88;
 let descontoMaxIR = 0.275;
+let parcelaAbate;
 let quantidadeDescontadaIR;
 let quantidadeDescontadaINSS;
-let quantidadeDescontadaINSS2;
+
+if (salarioBruto <= 1556.94) {
+  descontoINSS = 0.08;
+} else if (salarioBruto <= 2594.92 && salarioBruto > 1556.94) {
+  descontoINSS = 0.09;
+} else if (salarioBruto <= 5189.82 && salarioBruto > 2594.92) {
+  descontoINSS = 0.11;
+} else {
+  descontoINSS = 0;
+}
+
+quantidadeDescontadaINSS = (salarioBruto * descontoINSS); 
+if (quantidadeDescontadaINSS === 0) {
+  quantidadeDescontadaINSS = descontoMaxINSS;
+}
+
+salarioBruto -= quantidadeDescontadaINSS;
 
 if (salarioBruto <= 1903.98) {
   descontoIR;
 } else if (salarioBruto <= 2826.65 && salarioBruto > 1903.98) {
   descontoIR = 0.075;
-  // console.log("+ parcela de R$ 142,80 a deduzir do imposto");
+  parcelaAbate = 142.8;
 } else if (salarioBruto <= 3751.05 && salarioBruto > 2826.65) {
   descontoIR = 0.15;
-  // console.log("+ parcela de R$ 354,80 a deduzir do imposto");
+  parcelaAbate = 354.8;
 } else if (salarioBruto <= 4664.68 && salarioBruto > 3751.05) {
   descontoIR = 0.225;
-  // console.log("+ parcela de R$ 636,13 a deduzir do imposto");
+  parcelaAbate = 636.13;
 } else {
   descontoIR = descontoMaxIR;
-  // console.log("+ parcela de R$ 869,36 a deduzir do imposto");
-}
-quantidadeDescontadaIR = salarioBruto * descontoIR; 
-
-if (salarioBruto <= 1556.94) {
-  descontoINSS = 0.08;
-  quantidadeDescontadaINSS = salarioBruto * descontoINSS;
-  quantidadeDescontadaINSS2 = 0;
-} else if (salarioBruto <= 2594.92 && salarioBruto > 1556.94) {
-  descontoINSS = 0.09;
-  quantidadeDescontadaINSS = salarioBruto * descontoINSS;
-  quantidadeDescontadaINSS2 = 0;
-} else if (salarioBruto <= 5189.82 && salarioBruto > 2594.92) {
-  descontoINSS = 0.11;
-  quantidadeDescontadaINSS = salarioBruto * descontoINSS;
-  quantidadeDescontadaINSS2 = 0;
-} else {
-  quantidadeDescontadaINSS = 0;
-  quantidadeDescontadaINSS2 = descontoMaxINSS;
+  parcelaAbate = 869.36;
 }
 
-salarioLiquido = salarioBruto - (quantidadeDescontadaINSS + quantidadeDescontadaINSS2 + quantidadeDescontadaIR);
+quantidadeDescontadaIR = (salarioBruto * descontoIR) - parcelaAbate;
+
+salarioLiquido = salarioBruto - quantidadeDescontadaIR;
 
 console.log("Se o seu salário bruto for R$: ");
-console.log(salarioBruto);
+console.log(salarioCarteira);
 console.log("o seu salário líquido será de R$: ");
 console.log(salarioLiquido);
