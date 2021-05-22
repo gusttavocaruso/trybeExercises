5ee17598950ee0b67410047056b95a3030f94503
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-05-22 18:47:53 -0300

    add+

--- MODULO_01/BLOCO.4/4.2/exercíciosConteudo.js
+++ MODULO_01/BLOCO.4/4.2/exercíciosConteudo.js
@@ -1,17 +1,17 @@
-// EX1
-// let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
-// let menuServices = menu[1];
+EX1
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu[1];
 
-// console.log(menuServices);
+console.log(menuServices);
 
-// EX2
-// let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
-// let menuServices = menu.indexOf('Portifólio');
+EX2
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu.indexOf('Portifólio');
 
-// console.log(menuServices);
+console.log(menuServices);
 
-// EX3
-// let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
-// let menuServices = menu.push('Contato');
+EX3
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu.push('Contato');
 
-// console.log(menu);
+console.log(menu);
