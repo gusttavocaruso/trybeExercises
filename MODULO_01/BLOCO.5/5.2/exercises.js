529a6a20b0efc872dccdc413cf432ccefac1517d
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-05-28 13:40:21 -0300

    Corrige erro de digitação em h1 do arquivo .js

--- MODULO_01/BLOCO.5/5.2/exercises.js
+++ MODULO_01/BLOCO.5/5.2/exercises.js
@@ -1,6 +1,6 @@
 // 1.
 let h1 = document.createElement('h1');
-h1.innerText = 'Exercício 5.1 - JavaScript DOM';
+h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(h1);
 
 // 2.
