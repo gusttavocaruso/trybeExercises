9b78cd303cc033da2db79b64e1bd2d56572a0949
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-10 15:00:39 -0300

    add exercises course 6.1day

---
+++ MODULO_01/BLOCO.6/6.1/form.js
@@ -0,0 +1,9 @@
+const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+for (let i = 0; i < estados.length; i += 1) {
+  let opt = document.createElement('option');
+  opt.innerText = estados[i];
+  opt.required;
+  let states = document.getElementById('state');
+  states.appendChild(opt);
+}
