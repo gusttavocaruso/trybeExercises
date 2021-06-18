8602917218044e208949b45cff38815300c29b33
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-18 15:29:39 -0300

    add update 7.1day exercises

--- MODULO_01/BLOCO.7/7.1/exercise-partII.js
+++ MODULO_01/BLOCO.7/7.1/exercise-partII.js
@@ -1,10 +1,25 @@
-// odio mortal de exercicio de fatorar numero
-let result = 1;
-
+// odio mortal de exercicio de fatorial 
 const fatorial = (x) => {
-  for (let i = 1; i <= x; i += 1) {
-   result *= x * (x - i);
+  if (x === 1) {
+    return 1;
+  } else {
+    return x * fatorial(x - 1)
   }
-  return result;
 }
 console.log(fatorial(4));
+
+
+
+// ex2.. unfinished yet
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+function longestWord(str) {
+  let words = str.split(' ');
+  for (let i = 0; i < words.length; i += 1) {
+    let big = words[i];
+    if (words[i].length > big.length) {
+      big = words[i];
+    }
+    console.log(big);
+    }
+  }
