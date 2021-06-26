a778992f57cb9986697fad08045b4881e2647cf1
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-26 18:26:46 -0300

    add 7.1day exercises partI update

--- MODULO_01/BLOCO.7/7.1/exercise-partI.js
+++ MODULO_01/BLOCO.7/7.1/exercise-partI.js
@@ -1,3 +1,4 @@
+// 1.
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -6,12 +7,14 @@ const testingScope = (escopo) => {
   } else {
     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
-  }
-}
+  };
+};
 testingScope(true);
 
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
-console.log(oddsAndEvens);
+// 2.
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
