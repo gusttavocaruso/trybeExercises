78556b7a466360fbb07771cad72dfcb796a51d36
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-06 18:40:55 -0300

    add 9th block and 9.1day content

---
+++ MODULO_01/BLOCO.9/9.1/content2.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
