957004ddb4ef8430aa8ddb60ac496c04b0d43645
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-21 19:32:33 -0300

    add 11.2day exercise

---
+++ MODULO_02/BLOCO.12/pokedex-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex 
+          pokemons={pokemons}
+        />
+      </div>
+
+    );
+  }
+
+}
+
+export default App;
