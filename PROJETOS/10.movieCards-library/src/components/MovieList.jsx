faca2c1e07febb9319241476275d50cd4c69ba06
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-29 20:18:57 -0300

    update project

--- PROJETOS/10.movieCards-library/src/components/MovieList.jsx
+++ PROJETOS/10.movieCards-library/src/components/MovieList.jsx
@@ -9,7 +9,8 @@ class MovieList extends React.Component {
 
     return (
       <div className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map((movie) =>
+          <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
--- PROJETOS/10.movieCards-library/src/components/Rating.jsx
+++ PROJETOS/10.movieCards-library/src/components/Rating.jsx
@@ -12,7 +12,6 @@ class Rating extends React.Component {
       </div>
     );
   }
-
 }
 
 Rating.propTypes = {
