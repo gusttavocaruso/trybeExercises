c4de464323f417370a5ca9af67b138a34a3f3ace
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-01 19:04:28 -0300

    add 13th block

---
+++ MODULO_02/BLOCO.13/13.1/fetch-joke-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import DadJoke from './components/DadJoke'
+
+function App() {
+  return (
+    <div className="App">
+      <DadJoke />
+    </div>
+  );
+}
+
+export default App;
