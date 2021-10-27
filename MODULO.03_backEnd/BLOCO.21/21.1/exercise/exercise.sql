737753e519ae2a17cb9a4ca04b75cef38c633c93
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-10-27 14:24:10 -0300

    add 21.1day exercise folder

---
+++ MODULO.03_backEnd/BLOCO.21/21.1/exercise/exercise.sql
@@ -0,0 +1,9 @@
+-- ex1. Um zoológico precisa de um banco de dados para armazenar informações sobre os seus animais. As informações a serem armazenadas sobre cada animal são:
+
+Nome;
+Espécie;
+Sexo;
+Idade;
+Localização.
+
+-- Cada animal também possui vários cuidadores, e cada cuidador pode ser responsável por mais de um animal. Além disso, cada cuidador possui um gerente, sendo que cada gerente pode ser responsável por mais de um cuidador.
