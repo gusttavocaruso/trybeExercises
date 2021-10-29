bff81ddd485b4834ddcabd8052f13dfa1881da71
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-10-29 13:15:44 -0300

    add ALTER_TABLE exercise

--- MODULO.03_backEnd/BLOCO.21/21.3/exercises/exercise_ALTER_TABLE.sql
+++ MODULO.03_backEnd/BLOCO.21/21.3/exercises/exercise_ALTER_TABLE.sql
@@ -1,7 +1,18 @@
 -- Desafios sobre ALTER TABLE ============
 
--- ex1 Escreva uma query SQL para alterar na tabela localtions o nome da coluna street_address para address , mantendo o mesmo tipo e tamanho de dados.
+-- ex1 Escreva uma query SQL para alterar na tabela localtions o nome da coluna street_address
+-- para address , mantendo o mesmo tipo e tamanho de dados.
+USE hr;
+ALTER TABLE locations CHANGE street_address ADDRESS VARCHAR(45) NOT NULL;
 
--- ex2 Escreva uma query SQL para alterar o nome da coluna region_name para region , mantendo o mesmo tipo e tamanho de dados.
 
--- ex3 Escreva uma query SQL para alterar o nome da coluna country_name para country , mantendo o mesmo tipo e tamanho de dados.
+-- ex2 Escreva uma query SQL para alterar o nome da coluna region_name para region , 
+-- mantendo o mesmo tipo e tamanho de dados.
+USE hr;
+ALTER TABLE regions CHANGE region_name REGION VARCHAR(25) UNIQUE;
+
+
+-- ex3 Escreva uma query SQL para alterar o nome da coluna country_name para country , 
+-- mantendo o mesmo tipo e tamanho de dados.
+USE hr;
+ALTER TABLE countries CHANGE COUNTRY_NAME COUNTRY VARCHAR(40) NOT NULL;
