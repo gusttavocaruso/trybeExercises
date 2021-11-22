fd102a47e3bbdcaf033fa43f973296609139c81e
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-11-22 17:43:48 -0300

    update in currentDate file

--- MODULO.03_backEnd/BLOCO.24/24.1/content/$currentDate.js
+++ MODULO.03_backEnd/BLOCO.24/24.1/content/$currentDate.js
@@ -7,3 +7,7 @@ db.collection.updateOne(
   },
 );
 // código acim acriará no DB um chave "updated" que dentro tem uma chave "data" e que dentro terá uma chave "$date" com o timestamp atual
+
+// um valor booleano true para atribuir o valor da data corrente ao campo utilizando o tipo Date;
+// OU
+// um documento que especifica o tipo do campo. Esse documento pode ser { $type: "timestamp" } ou { $type: "date" } . Esse operador é case-sensitive e aceita somente letras minúsculas: timestamp ou date .
