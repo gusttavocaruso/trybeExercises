763d24e6235c91f281732c7b78d7d4d9a5e8b96b
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-11-23 12:16:49 -0300

    update in project folder

--- PROJETOS/17.starWars/src/service/planetAPI.js
+++
@@ -1,7 +0,0 @@
-const planetAPI = async () => {
-  const API = 'https://swapi-trybe.herokuapp.com/api/planets/';
-  const { results } = await fetch(API).then((resp) => resp.json());
-  return results;
-};
-
-export default planetAPI;
