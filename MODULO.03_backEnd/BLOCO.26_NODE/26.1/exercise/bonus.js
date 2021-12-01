d188212dca0e3ecec86ba70b82e89447a477aa97
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-12-01 13:57:38 -0300

    add 26.1exercise-day

---
+++ MODULO.03_backEnd/BLOCO.26_NODE/26.1/exercise/bonus.js
@@ -0,0 +1,19 @@
+
+// Crie um script que realize o fatorial de um número n .
+// O fatorial é a multiplicação de um número por todos os seus antecessores até chegar ao número 1.
+// Armazene o script no arquivo fatorial.js .
+// Utilize o readline-sync para realizar o input de dados.
+// O script deve ser acionado através do comando npm run fatorial
+// Adicione o script ao menu criado no exercício 5.
+// Adicione validações para garantir que o valor informado seja um inteiro maior que 0.
+
+
+// Crie um script que exiba o valor dos n primeiros elementos da sequência de fibonacci.
+// A sequência de fibonacci começa com 0 e 1 e os números seguintes são sempre a soma dos dois números anteriores.
+// Armazene o script no arquivo fibonacci.js .
+// Utilize o readline-sync para realizar o input de dados.
+// O script deve ser acionado através do comando npm run fibonacci
+// Adicione o script ao menu criado no exercício 5.
+// Não imprima o valor 0 , uma vez que ele não está incluso na sequência;
+// Quando n = 10 , exatamente 10 elementos devem ser exibidos;
+// Adicione validações para garantir que o valor informado é um inteiro maior que 0.
