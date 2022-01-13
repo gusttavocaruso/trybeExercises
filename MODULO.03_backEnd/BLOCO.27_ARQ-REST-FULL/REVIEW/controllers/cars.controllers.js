ebca40dddfd4e6d600cd53231eb1406f699aeb7b
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-01-13 11:49:08 -0300

    add update

--- MODULO.03_backEnd/BLOCO.27_ARQ-REST-FULL/REVIEW/controllers/cars.controllers.js
+++ MODULO.03_backEnd/BLOCO.27_ARQ-REST-FULL/REVIEW/controllers/cars.controllers.js
@@ -1,13 +1,13 @@
 const { createCar } = require('../services/car.services');
 
-const carCreate = async (req, res, _next) => {
+const carCreate = async (req, res, next) => {
   try {
     const CAR = req.body;
     const newCar = await createCar(CAR);
     return res.status(200).json(newCar);
   } catch (error) {
     console.log(`POST CREATECAR -> ${error.message}`);
-    return _next(error)
+    return next(error)
   }
 };
 
--- MODULO.03_backEnd/BLOCO.27_ARQ-REST-FULL/REVIEW/models/cars.models.js
+++ MODULO.03_backEnd/BLOCO.27_ARQ-REST-FULL/REVIEW/models/cars.models.js
@@ -1,4 +1,3 @@
-const { ObjectId } = require('bson');
 const connect = require('./connections');
 
 const create = async ({ marca, modelo, ano, cor, placa }) => {
