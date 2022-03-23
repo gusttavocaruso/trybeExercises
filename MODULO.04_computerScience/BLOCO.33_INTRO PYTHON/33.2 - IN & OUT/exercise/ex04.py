97b925fc419bd62df6e0b5608f0a85639265277b
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-03-23 20:35:46 -0300

    add new file on 33.2 folder

--- MODULO.04_computerScience/BLOCO.33_INTRO PYTHON/33.2 - IN & OUT/exercise/ex04.py
+++ MODULO.04_computerScience/BLOCO.33_INTRO PYTHON/33.2 - IN & OUT/exercise/ex04.py
@@ -6,14 +6,26 @@
 # Java,0.23201856148491878
 # PHP,0.23201856148491878
 
-
 import json
+
 
 def import_books(file_path):
     with open(file_path) as file:
         return [json.loads(line) for line in file]
 
 
+def count_books_by_categories(books):
+    categories = {}
+    for book in books:
+        for category in book['categories']:
+            if not categories.get(category):
+                categories[category] = 0
+            categories[category] += 1
+    # print(categories)
+    return categories
+
+
 if __name__ == '__main__':
     books = import_books('books.json')
-    print(books)
+    count_books_by_categories(books)
+    # print(len(books))
